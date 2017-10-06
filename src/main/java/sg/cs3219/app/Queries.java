87d98b25754a7f53cd2c61164863dd142acc3550
Apoorva Ullas <apoorva_ullas@yahoo.com.sg>
2017-10-06 21:46:07 +0800

    Add a return type to all methods + add valid check for citations for first 8 queries

--- src/main/java/sg/cs3219/app/Queries.java
+++ src/main/java/sg/cs3219/app/Queries.java
@@ -21,14 +21,8 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class Queries {
-	private static int totalCitations = 0;
-	private static int totalCitedAuthors = 0;
-	private static Set<String> uniqueCitationSet = new HashSet<String>();
-	private static int[] rangeOfYears = new int[2];
+	private static int totalDocuments = 0;
 	private static ArrayList<String> XMLpathList = new ArrayList<String>();
-	private static int[] numberOfCitedDocumentsD12 = new int[16];
-	private static int[] numberOfCitedDocumentsD13 = new int[2];
-	private static int[] numberOfCitedDocumentsYoshuaBengio = new int[16];
 	private static int[][] numberOfCitedDocumentsPerYears = new int[4][16];
 	private static final int START_YEAR = 2000;
 	private static final int END_YEAR = 2015;
@@ -40,27 +34,48 @@ public class Queries {
 	public static void main(String[] args) {
 		String path = "src/main/resources";
 		initializations();
-		findTotalDocuments(path);
-		System.out.println("Q1: Total documents: " + XMLpathList.size());
-		findTotalCitations();
+
+		// QUERY 1
+		int totalDocs = findTotalDocuments(path);
+		System.out.println("Q1: Total documents: " + totalDocs);
+
+		// QUERY 2
+		int totalCitations = findTotalCitations();
 		System.out.println("Q2: Total citations: " + totalCitations);
-		System.out.println("Q3: Total unique citations: " + uniqueCitationSet.size());
-		findTotalAuthorsCited();
+
+		// QUERY 3
+		int totalUniqueCitations = findUniqueCitations();
+		System.out.println("Q3: Total unique citations: " + totalUniqueCitations);
+
+		// QUERY 4
+		int totalCitedAuthors = findTotalAuthorsCited();
 		System.out.println("Q4: Total authors in citations: " + totalCitedAuthors);
-		findRangeOfYears();
+
+		// QUERY 5
+		int[] rangeOfYears = new int[2];
+		rangeOfYears = findRangeOfYears();
 		System.out.println("Q5: Range of Years of cited documents: " + rangeOfYears[0] + " - " + rangeOfYears[1]);
+
+		// QUERY 6
+		int[] numberOfCitedDocumentsD12 = new int[16];
+		numberOfCitedDocumentsD12 = countYearOfCitedDocumentD12();
 		System.out.println("Q6: Number of cited documents published in following years in D12: ");
-		countYearOfCitedDocumentD12();
 		int initialYear = START_YEAR;
 		for (int i = 0; i < numberOfCitedDocumentsD12.length - 1; i++) {
 			System.out.print(initialYear + ": " + numberOfCitedDocumentsD12[i] + " , ");
 			initialYear++;
 		}
 		System.out.println(END_YEAR + ": " + numberOfCitedDocumentsD12[15]);
-		countNumberOfCitedDocumentsD13();
+
+		// QUERY 7
+		int[] numberOfCitedDocumentsD13 = new int[16];
+		numberOfCitedDocumentsD13 = countNumberOfCitedDocumentsD13();
 		System.out.println("Q7: Number of cited documents published in EMNLP: " + numberOfCitedDocumentsD13[0]
 				+ " CoNLL: " + numberOfCitedDocumentsD13[1]);
-		findTotalYoshuaBengio();
+
+		// QUERY 8
+		int[] numberOfCitedDocumentsYoshuaBengio = new int[16];
+		numberOfCitedDocumentsYoshuaBengio = findTotalYoshuaBengio();
 		System.out.println("Q8: Number of cited documents published in following years by Yoshua Bengio: ");
 		initialYear = START_YEAR;
 		for (int i = 0; i < numberOfCitedDocumentsYoshuaBengio.length - 1; i++) {
@@ -68,42 +83,31 @@ public class Queries {
 			initialYear++;
 		}
 		System.out.println(END_YEAR + ": " + numberOfCitedDocumentsYoshuaBengio[15]);
+
+		// QUERY 9
 		findPerYear();
 		System.out.println("Q9: number of cited documents published in each of the years from 2010 to 2015: ");
-		for(int i = 0; i <numberOfCitedDocumentsPerYears.length;i++){
-			for(int j=0;j<numberOfCitedDocumentsPerYears[0].length;j++){
-				System.out.print(
-						 (j+2000)+ " " + numberOfCitedDocumentsPerYears[i][j]+ " "
-						);
+		for (int i = 0; i < numberOfCitedDocumentsPerYears.length; i++) {
+			for (int j = 0; j < numberOfCitedDocumentsPerYears[0].length; j++) {
+				System.out.print((j + 2000) + " " + numberOfCitedDocumentsPerYears[i][j] + " ");
 			}
 		}
+
 	}
 
 	// INITIALIZATIONS
 	private static void initializations() {
-		rangeOfYears[0] = 3000;
-		rangeOfYears[1] = 0;
-		for (int i = 0; i < numberOfCitedDocumentsD12.length; i++) {
-			numberOfCitedDocumentsD12[i] = 0;
-		}
-		for (int j = 0; j < numberOfCitedDocumentsD13.length; j++) {
-			numberOfCitedDocumentsD13[j] = 0;
-		}
-		for (int k = 0; k < numberOfCitedDocumentsYoshuaBengio.length; k++) {
-			numberOfCitedDocumentsYoshuaBengio[k] = 0;
-		}
-		for(int i = 0; i <numberOfCitedDocumentsPerYears.length;i++){
-			for(int j=0;j<numberOfCitedDocumentsPerYears[0].length;j++){
-				numberOfCitedDocumentsPerYears[i][j]=0;
+		for (int i = 0; i < numberOfCitedDocumentsPerYears.length; i++) {
+			for (int j = 0; j < numberOfCitedDocumentsPerYears[0].length; j++) {
+				numberOfCitedDocumentsPerYears[i][j] = 0;
 			}
 		}
 	}
 
 	// QUERY 1
-	private static void findTotalDocuments(String path) {
+	private static int findTotalDocuments(String path) {
 		File f = new File(path);
 		File[] files = f.listFiles();
-
 		if (files != null) {
 			for (int i = 0; i < files.length; i++) {
 				File file = files[i];
@@ -111,21 +115,31 @@ public class Queries {
 					findTotalDocuments(file.getAbsolutePath());
 				} else {
 					XMLpathList.add(file.getAbsolutePath());
+					totalDocuments++;
 				}
 			}
 		}
+		return totalDocuments;
 	}
 
 	// QUERY 2
-	private static void findTotalCitations() {
+	private static int findTotalCitations() {
+		int totalCitations = 0;
 		for (String filepath : XMLpathList) {
 			try {
 				DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 				DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 				Document doc = docBuilder.parse(filepath);
-				NodeList list = doc.getElementsByTagName("citation");
-				getUniqueCitations(list);
-				totalCitations += list.getLength();
+				NodeList citations = doc.getElementsByTagName("citation");
+				for (int i = 0; i < citations.getLength(); i++) {
+					if (citations.item(i).getNodeType() == Node.ELEMENT_NODE) {
+						Element citation = (Element) citations.item(i);
+						if (!citation.getAttribute("valid").equals("true")) {
+							continue;
+						}
+						totalCitations++;
+					}
+				}
 			} catch (ParserConfigurationException pce) {
 				pce.printStackTrace();
 			} catch (IOException ioe) {
@@ -134,24 +148,27 @@ public class Queries {
 				sae.printStackTrace();
 			}
 		}
+		return totalCitations;
 	}
 
 	// QUERY 3
-	private static void getUniqueCitations(NodeList list) {
-		for (int i = 0; i < list.getLength(); i++) {
-			uniqueCitationSet.add(list.item(i).getTextContent());
-		}
-	}
-
-	// QUERY 4
-	private static void findTotalAuthorsCited() {
+	private static int findUniqueCitations() {
+		Set<String> uniqueCitationSet = new HashSet<String>();
 		for (String filepath : XMLpathList) {
 			try {
 				DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 				DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 				Document doc = docBuilder.parse(filepath);
-				NodeList list = doc.getElementsByTagName("authors");
-				totalCitedAuthors += list.getLength();
+				NodeList citations = doc.getElementsByTagName("citation");
+				for (int i = 0; i < citations.getLength(); i++) {
+					if (citations.item(i).getNodeType() == Node.ELEMENT_NODE) {
+						Element citation = (Element) citations.item(i);
+						if (!citation.getAttribute("valid").equals("true")) {
+							continue;
+						}
+						uniqueCitationSet.add(citation.getTextContent());
+					}
+				}
 			} catch (ParserConfigurationException pce) {
 				pce.printStackTrace();
 			} catch (IOException ioe) {
@@ -160,26 +177,27 @@ public class Queries {
 				sae.printStackTrace();
 			}
 		}
+		return uniqueCitationSet.size();
 	}
 
-	// QUERY 5
-	private static void findRangeOfYears() {
+	// QUERY 4
+	private static int findTotalAuthorsCited() {
+		Set<String> authorSet = new HashSet<String>();
 		for (String filepath : XMLpathList) {
 			try {
 				DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 				DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 				Document doc = docBuilder.parse(filepath);
-				NodeList list = doc.getElementsByTagName("date");
-				for (int i = 0; i < list.getLength(); i++) {
-					if (!list.item(i).hasAttributes()) {
-						String date = list.item(i).getTextContent().toString();
-						if (date != "") {
-							int year = Integer.parseInt(date);
-							if (year < rangeOfYears[0]) {
-								rangeOfYears[0] = year;
-							} else if (year > rangeOfYears[1]) {
-								rangeOfYears[1] = year;
-							}
+				NodeList citations = doc.getElementsByTagName("citation");
+				for (int i = 0; i < citations.getLength(); i++) {
+					if (citations.item(i).getNodeType() == Node.ELEMENT_NODE) {
+						Element citation = (Element) citations.item(i);
+						if (!citation.getAttribute("valid").equals("true")) {
+							continue;
+						}
+						NodeList authors = citation.getElementsByTagName("author");
+						for (int j = 0; j < authors.getLength(); j++) {
+							authorSet.add(authors.item(j).getTextContent());
 						}
 					}
 				}
@@ -191,27 +209,89 @@ public class Queries {
 				sae.printStackTrace();
 			}
 		}
+		return authorSet.size();
+	}
+
+	// QUERY 5
+	private static int[] findRangeOfYears() {
+		int[] rangeOfYears = new int[2];
+		rangeOfYears[0] = 3000;
+		rangeOfYears[1] = 0;
+		for (String filepath : XMLpathList) {
+			try {
+				DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+				Document doc = docBuilder.parse(filepath);
+				NodeList citations = doc.getElementsByTagName("citation");
+				for (int i = 0; i < citations.getLength(); i++) {
+					if (citations.item(i).getNodeType() == Node.ELEMENT_NODE) {
+						Element citation = (Element) citations.item(i);
+						if (!citation.getAttribute("valid").equals("true")) {
+							continue;
+						}
+						NodeList dates = citation.getElementsByTagName("date");
+						for (int j = 0; j < dates.getLength(); j++) {
+							if (!dates.item(j).hasAttributes()) {
+								String date = dates.item(j).getTextContent();
+								if (date != "") {
+									int year = Integer.parseInt(date);
+									if (year < rangeOfYears[0]) {
+										rangeOfYears[0] = year;
+									} else if (year > rangeOfYears[1]) {
+										rangeOfYears[1] = year;
+									}
+								}
+							}
+						}
+
+					}
+				}
+			} catch (ParserConfigurationException pce) {
+				pce.printStackTrace();
+			} catch (IOException ioe) {
+				ioe.printStackTrace();
+			} catch (SAXException sae) {
+				sae.printStackTrace();
+			}
+		}
+
+		return rangeOfYears;
 	}
 
 	// QUERY 6
-	private static void countYearOfCitedDocumentD12() {
+	private static int[] countYearOfCitedDocumentD12() {
+		int[] numberOfCitedDocumentsD12 = new int[16];
+		for (int i = 0; i < numberOfCitedDocumentsD12.length; i++) {
+			numberOfCitedDocumentsD12[i] = 0;
+		}
+
 		for (String filepath : XMLpathList) {
 			if (filepath.contains("D12")) {
 				try {
 					DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 					DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 					Document doc = docBuilder.parse(filepath);
-					NodeList list = doc.getElementsByTagName("date");
-					for (int i = 0; i < list.getLength(); i++) {
-						if (!list.item(i).hasAttributes()) {
-							String date = list.item(i).getTextContent().toString();
-							if (date != "") {
-								int year = Integer.parseInt(date);
-								if ((year >= START_YEAR) && (year <= END_YEAR)) {
-									int x = year % 2000;
-									numberOfCitedDocumentsD12[x]++;
+					NodeList citations = doc.getElementsByTagName("citation");
+					for (int i = 0; i < citations.getLength(); i++) {
+						if (citations.item(i).getNodeType() == Node.ELEMENT_NODE) {
+							Element citation = (Element) citations.item(i);
+							if (!citation.getAttribute("valid").equals("true")) {
+								continue;
+							}
+							NodeList dates = citation.getElementsByTagName("date");
+							for (int j = 0; j < dates.getLength(); j++) {
+								if (!dates.item(j).hasAttributes()) {
+									String date = dates.item(j).getTextContent();
+									if (date != "") {
+										int year = Integer.parseInt(date);
+										if ((year >= START_YEAR) && (year <= END_YEAR)) {
+											int x = year % 2000;
+											numberOfCitedDocumentsD12[x]++;
+										}
+									}
 								}
 							}
+
 						}
 					}
 				} catch (ParserConfigurationException pce) {
@@ -223,23 +303,38 @@ public class Queries {
 				}
 			}
 		}
+
+		return numberOfCitedDocumentsD12;
 	}
 
 	// QUERY 7
-	private static void countNumberOfCitedDocumentsD13() {
+	private static int[] countNumberOfCitedDocumentsD13() {
+		int[] numberOfCitedDocumentsD13 = new int[2];
+		for (int j = 0; j < numberOfCitedDocumentsD13.length; j++) {
+			numberOfCitedDocumentsD13[j] = 0;
+		}
 		for (String filepath : XMLpathList) {
 			if (filepath.contains("D13")) {
 				try {
 					DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 					DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 					Document doc = docBuilder.parse(filepath);
-					NodeList list = doc.getElementsByTagName("booktitle");
-					for (int i = 0; i < list.getLength(); i++) {
-						String title = list.item(i).getTextContent().toString().toUpperCase();
-						if ((title.contains("EMNLP")) || (title.contains(EMNLP_FULL_FORM))) {
-							numberOfCitedDocumentsD13[0]++;
-						} else if ((title.contains("CONLL")) || (title.contains(CONLL_FULL_FORM))) {
-							numberOfCitedDocumentsD13[1]++;
+					NodeList citations = doc.getElementsByTagName("citation");
+					for (int i = 0; i < citations.getLength(); i++) {
+						if (citations.item(i).getNodeType() == Node.ELEMENT_NODE) {
+							Element citation = (Element) citations.item(i);
+							if (!citation.getAttribute("valid").equals("true")) {
+								continue;
+							}
+							NodeList booktitles = citation.getElementsByTagName("booktitle");
+							for (int k = 0; k < booktitles.getLength(); k++) {
+								String title = booktitles.item(k).getTextContent().toUpperCase();
+								if ((title.contains("EMNLP")) || (title.contains(EMNLP_FULL_FORM))) {
+									numberOfCitedDocumentsD13[0]++;
+								} else if ((title.contains("CONLL")) || (title.contains(CONLL_FULL_FORM))) {
+									numberOfCitedDocumentsD13[1]++;
+								}
+							}
 						}
 					}
 				} catch (ParserConfigurationException pce) {
@@ -251,34 +346,51 @@ public class Queries {
 				}
 			}
 		}
+		return numberOfCitedDocumentsD13;
 	}
 
 	// QUERY 8
-	private static void findTotalYoshuaBengio() {
+	private static int[] findTotalYoshuaBengio() {
+		int[] numberOfCitedDocumentsYoshuaBengio = new int[16];
+		for (int k = 0; k < numberOfCitedDocumentsYoshuaBengio.length; k++) {
+			numberOfCitedDocumentsYoshuaBengio[k] = 0;
+		}
+
 		for (String filepath : XMLpathList) {
 			try {
 				DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 				DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 				Document doc = docBuilder.parse(filepath);
-				NodeList list = doc.getElementsByTagName("citation");
-				for (int i = 0; i < list.getLength(); i++) {
-					String str = list.item(i).getTextContent().toUpperCase();
-					if ((str.contains(AUTHOR_FULL_FORM)) || (str.contains(AUTHOR_SHORT_FORM))) {
-						NodeList listChild = list.item(i).getChildNodes();
-						for (int j = 0; j < listChild.getLength(); j++) {
-							if (listChild.item(j).getNodeName() == "date") {
-								String date = listChild.item(j).getTextContent();
-								if (date != "") {
-									int year = Integer.parseInt(date);
-									if ((year >= START_YEAR) && (year <= END_YEAR)) {
-										int x = year % 2000;
-										numberOfCitedDocumentsYoshuaBengio[x]++;
+				NodeList citations = doc.getElementsByTagName("citation");
+				for (int i = 0; i < citations.getLength(); i++) {
+					if (citations.item(i).getNodeType() == Node.ELEMENT_NODE) {
+						Element citation = (Element) citations.item(i);
+						if (!citation.getAttribute("valid").equals("true")) {
+							continue;
+						}
+						NodeList authors = citation.getElementsByTagName("author");
+						for (int j = 0; j < authors.getLength(); j++) {
+							String author = authors.item(j).getTextContent().toUpperCase();
+							if ((author.equals(AUTHOR_FULL_FORM)) || (author.equals(AUTHOR_SHORT_FORM))) {
+								NodeList dates = citation.getElementsByTagName("date");
+								for (int m = 0; m < dates.getLength(); m++) {
+									if (!dates.item(m).hasAttributes()) {
+										String date = dates.item(m).getTextContent();
+										if (date != "") {
+											int year = Integer.parseInt(date);
+											if ((year >= START_YEAR) && (year <= END_YEAR)) {
+												int x = year % 2000;
+												numberOfCitedDocumentsYoshuaBengio[x]++;
+											}
+										}
 									}
 								}
 							}
 						}
+
 					}
 				}
+
 			} catch (ParserConfigurationException pce) {
 				pce.printStackTrace();
 			} catch (IOException ioe) {
@@ -287,6 +399,8 @@ public class Queries {
 				sae.printStackTrace();
 			}
 		}
+
+		return numberOfCitedDocumentsYoshuaBengio;
 
 	}
 
@@ -303,37 +417,34 @@ public class Queries {
 					for (int i = 0; i < citations.getLength(); i++) {
 						if (citations.item(i).getNodeType() == Node.ELEMENT_NODE) {
 							Element citation = (Element) citations.item(i);
-							
+
 							if (!citation.getAttribute("valid").equals("true")) {
 								continue;
 							}
-							
+
 							NodeList yearEl = citation.getElementsByTagName("date");
-							if(yearEl.getLength() > 0){
+							if (yearEl.getLength() > 0) {
 								String year = yearEl.item(0).getTextContent();
 								if (year == "") {
 									continue;
 								}
 								int y = Integer.parseInt(year) - 2000;
-								if(y<0 || y>15){
+								if (y < 0 || y > 15) {
 									continue;
 								}
 								String confName = filepath.split("/")[4];
-								if(filepath.contains("J14"))
-									numberOfCitedDocumentsPerYears[0][y] ++;
-								
-								if(filepath.contains("J14"))
-									numberOfCitedDocumentsPerYears[1][y] ++;
-									
-								if(filepath.contains("J14"))
-									numberOfCitedDocumentsPerYears[2][y] ++;
-									
-								if(filepath.contains("J14"))
-									numberOfCitedDocumentsPerYears[3][y] ++;
-									
-									
-								
-								
+								if (filepath.contains("J14"))
+									numberOfCitedDocumentsPerYears[0][y]++;
+
+								if (filepath.contains("J14"))
+									numberOfCitedDocumentsPerYears[1][y]++;
+
+								if (filepath.contains("J14"))
+									numberOfCitedDocumentsPerYears[2][y]++;
+
+								if (filepath.contains("J14"))
+									numberOfCitedDocumentsPerYears[3][y]++;
+
 							}
 
 						}
